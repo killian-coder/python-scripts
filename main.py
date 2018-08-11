b683e3e1c0b09a9c16a89d8741321c83fdf14fef
killian-coder <chintalima49@gmail.com>
2018-08-11 21:22:16 +0200

    other python scripts for keeping

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+fw = open('sample.txt','w')
+fw.write('writing some stuff in my text file\n')
+fw.write('i like becon and tuna')
+fw.close()
+
+fr = open('sample.txt','r')
+text = fr.read()
+print(text)
+fr.close()
+'''
+import math
+stan = ['stan smith', '24', 4000, 'lusaka']
+paige = ['paige walters','21',3500,'kitwe']
+stan[2] += 800
+print(stan[2])
+
+
+
+
---
+++ sms/Sms.py
@@ -0,0 +1,7 @@
+﻿from twilio.rest import TwilioRestClient
+
+client = TwilioRestClient()
+
+client.messages.create(from_="(260) 976-127357",
+                       to="(260) 976-127357",
+                       body=" hey there from twilio!")
